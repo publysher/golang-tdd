2b6bea4066c8e66cea67f82ba8f254332cb8d882
Yigal Duppen <yigal@publysher.nl>
2014-01-26 13:02:56 +0100

    A first skeleton for generating QR codes

---
+++ qrcoded.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	fmt.Println("Hello QR Code")
+
+	qrcode := GenerateQRCode("555-2368")
+	ioutil.WriteFile("qrcode.png", qrcode, 0644)
+}
+
+func GenerateQRCode(code string) []byte {
+	return nil
+}
