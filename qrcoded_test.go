1a82ec342bded60017631e52b3ba2f00040abd31
Yigal Duppen <yigal@publysher.nl>
2014-02-05 16:21:50 +0100

    Improved test
    * Combined two happy paths into one
    * Test could not handle <nil> errors

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -7,18 +7,14 @@ import (
 	"testing"
 )
 
-func TestGenerateQRCodeReturnsValue(t *testing.T) {
+func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 	buffer := new(bytes.Buffer)
 	GenerateQRCode(buffer, "555-2368")
 
 	if buffer.Len() == 0 {
 		t.Errorf("No QRCode generated")
 	}
-}
 
-func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
-	buffer := new(bytes.Buffer)
-	GenerateQRCode(buffer, "555-2368")
 	_, err := png.Decode(buffer)
 
 	if err != nil {
@@ -28,7 +24,7 @@ func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 
 type ErrorWriter struct{}
 
-func (e ErrorWriter) Write(b []byte) (int, error) {
+func (e *ErrorWriter) Write(b []byte) (int, error) {
 	return 0, errors.New("Expected error")
 }
 
@@ -37,6 +33,6 @@ func TestGenerateQRCodePropagatesErrors(t *testing.T) {
 	err := GenerateQRCode(w, "555-2368")
 
 	if err == nil || err.Error() != "Expected error" {
-		t.Errorf("Error not propagated correctly, got %s", err.Error())
+		t.Errorf("Error not propagated correctly, got %v", err)
 	}
 }
