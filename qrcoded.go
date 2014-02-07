e047caca430502af57a801d66427151828b127c1
Yigal Duppen <yigal@publysher.nl>
2014-02-07 11:49:48 +0100

    Introduce Versions

--- qrcoded.go
+++ qrcoded.go
@@ -17,13 +17,20 @@ func main() {
 	}
 	defer file.Close()
 
-	err = GenerateQRCode(file, "555-2368")
+	err = GenerateQRCode(file, "555-2368", Version(1))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func GenerateQRCode(w io.Writer, code string) error {
-	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
+func GenerateQRCode(w io.Writer, code string, version Version) error {
+	size := version.PatternSize()
+	img := image.NewNRGBA(image.Rect(0, 0, size, size))
 	return png.Encode(w, img)
+}
+
+type Version int8
+
+func (v Version) PatternSize() int {
+	return 4*int(v) + 17
 }
--- qrcoded_test.go
+++ qrcoded_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 	buffer := new(bytes.Buffer)
-	GenerateQRCode(buffer, "555-2368")
+	GenerateQRCode(buffer, "555-2368", Version(1))
 
 	if buffer.Len() == 0 {
 		t.Errorf("No QRCode generated")
@@ -30,9 +30,31 @@ func (e *ErrorWriter) Write(b []byte) (int, error) {
 
 func TestGenerateQRCodePropagatesErrors(t *testing.T) {
 	w := new(ErrorWriter)
-	err := GenerateQRCode(w, "555-2368")
+	err := GenerateQRCode(w, "555-2368", Version(1))
 
 	if err == nil || err.Error() != "Expected error" {
 		t.Errorf("Error not propagated correctly, got %v", err)
+	}
+}
+
+func TestVersionDeterminesSize(t *testing.T) {
+	table := []struct {
+		version  int
+		expected int
+	}{
+		{1, 21},
+		{2, 25},
+		{6, 41},
+		{7, 45},
+		{14, 73},
+		{40, 177},
+	}
+
+	for _, test := range table {
+		size := Version(test.version).PatternSize()
+		if size != test.expected {
+			t.Errorf("Version %2d, expected %3d but got %3d",
+				test.version, test.expected, size)
+		}
 	}
 }
